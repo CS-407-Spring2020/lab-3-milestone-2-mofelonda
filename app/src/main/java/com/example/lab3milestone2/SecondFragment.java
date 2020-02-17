7e8babfa77b579b1b9cbb61cfdefeb2684841d72
Mo Felonda <felonda@wisc.edu>
2020-02-17 15:14:29 -0600

    Done!

--- app/src/main/java/com/example/lab3milestone2/SecondFragment.java
+++ app/src/main/java/com/example/lab3milestone2/SecondFragment.java
@@ -7,7 +7,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
-import androidx.fragment.app.Fragment;
+import android.app.Fragment;
 
 public class SecondFragment extends Fragment {
 
